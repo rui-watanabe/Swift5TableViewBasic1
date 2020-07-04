a354153449773a09533ef2c4e71aa8b82af2d4fc
rui-watanabe <wataru610806@gmail.com>
2020-07-04 14:34:52 +0900

    deploy TableView

---
+++ NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5TableViewBasic
+//
+//  Created by watar on 2020/07/04.
+//  Copyright © 2020 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
