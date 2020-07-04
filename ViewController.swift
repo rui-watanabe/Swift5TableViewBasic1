869c8a76ff69987118cb1421b3626e78f530cf9d
rui-watanabe <wataru610806@gmail.com>
2020-07-04 16:17:05 +0900

    implement TableViewMethods

--- ViewController.swift
+++ ViewController.swift
@@ -18,16 +18,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
+        textField.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return textArray.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = textArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: "checkImage")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // When tapped, take out the contents of the array number and pass it
+        let nextVC = storyboard?.instantiateViewController(identifier: "next") as! NextViewController
+        
+        nextVC.toDoString = textArray[indexPath.row]
+        
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height / 6
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
